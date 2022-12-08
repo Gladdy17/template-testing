d65cf99a0a28ed8d73741266180d5173985ef93e
Gladston Pereira <exe@Gladstons-MacBook-Pro.local>
2022-12-07 20:24:38 -0700

    Eventlisters added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText =  document.getElementById('quote');
+const authorText =  document.getElementById('author');
+const twitterBtn =  document.getElementById('twitter');
+const newQuoteBtn =  document.getElementById('newquote');
+
+
+
 let apiQuotes = [];
 
 // Show New quote 
@@ -5,7 +13,21 @@ let apiQuotes = [];
 function newQuote(){
     // Pick a random Quote from Api Quote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if Author field is blank and replace it with 'Unkown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+   quoteText.textContent = quote.text;
+
 }
 
 // Get Quotes From API
@@ -20,6 +42,16 @@ async function getQuotes(){
     }
 
 }
+
+// Tweet a Quote 
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 
